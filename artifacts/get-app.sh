88ef1041eda50008928d65c332dc1caacb7ad85c
olayemio <olayemio@users.noreply.github.com>
2020-03-13 10:54:28 -0700

    Update get-app.sh

--- artifacts/get-app.sh
+++ artifacts/get-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 size=""
 ulimit -n 20000
-/usr/bin/wget -O /opt/server https://github.com/vmssignite/ignite2019/raw/controlcrash/server
+/usr/bin/wget -O /opt/server https://github.com/olayemio/azure-e2e-tutorial/raw/master/artifacts/server
 sleep 5
 chmod +x /opt/server
 /opt/server
